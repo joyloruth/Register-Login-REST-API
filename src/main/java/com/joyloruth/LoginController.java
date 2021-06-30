5d643ed559405cba996ebaac8d2df4af97160fcd
Joy LoRuth <joyloruth@gmail.com>
2021-06-29 21:41:22 -0500

    create Controller class
    added request mapping

---
+++ src/main/java/com/joyloruth/LoginController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.joyloruth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Joy LoRuth
+ *
+ */
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/")
+	public void checkMVC() {
+		
+	}
+
+}
