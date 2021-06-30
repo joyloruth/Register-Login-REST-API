6c652d604ca9abb14a95ae5e488e9ec8ce3968c7
Joy LoRuth <joyloruth@gmail.com>
2021-06-29 20:33:19 -0500

    Login/Register web app REST API in using Spring MVC

---
+++ src/main/java/com/joyloruth/RegisterLoginMvcApplication.java
@@ -0,0 +1,13 @@
+package com.joyloruth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterLoginMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterLoginMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joyloruth/RegisterLoginMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyloruth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterLoginMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
